3e69bf67281edadfa12a10988ba3a99e97a242ec
Abrahamlangat2021 <92026591+Abrahamlangat2021@users.noreply.github.com>
2021-10-23 16:12:26 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+Project:County library management system.
+Author:Abraham Langat.
+Date;oct 2021.
+Language:c99.
+License:mit.
+
+
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tcounty library management system\n");
+    printf("welcome Abraham.\n");
+    printf("what would you like to do?.\n");
+    printf("1.Add new user\n");
+    printf("2.Add book.\n");
+    printf("3.Search books.\n");
+    printf("4.Check list.\n");
+    printf("5.online access.\n");
+
+
+    printf("selected action");
+
+
+
+
+    return 0;
+}
